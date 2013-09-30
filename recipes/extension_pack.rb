cf2d3f0a2d00a9728b994958358c3f47d04fbb19
Ian Blenke <ian@blenke.com>
2013-09-29 22:37:34 -0400

    The include method works differently in a chef recipe

--- recipes/extension_pack.rb
+++ recipes/extension_pack.rb
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include Vbox::Helpers
 
 remote_file node['virtualbox']['extpack_file'] do
   source node['virtualbox']['extpack_url']
